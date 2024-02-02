dea62fdb6fd3e0a2a3d3413af5f600eecdf4efd6
Swastika_Pandit <swastikapandit02@gmail.com>
2024-02-02 16:43:08 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const indexRouter = require('./routers/indexRouters');
+
+app.use(bodyParser.urlencoded({ extended:false}));
+app.use(bodyParser.json());
+
+app.use('./api/books', indexRouter);
+
+module.exports = app;

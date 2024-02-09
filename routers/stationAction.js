71002e1d421843ac83c142f1ddd0e3ed275420e8
AN_SWAPNIL <a.n.swapnil2003@gmail.com>
2024-02-09 17:31:25 +0600

    station api updated

--- routers/stationAction.js
+++ routers/stationAction.js
@@ -1,7 +1,7 @@
 const express=require('express');
 
 const router=express.Router();
-const Station = require('./models/station'); // Import your Station model
+const Station = require('../models/station'); // Import your Station model
 
 
 router.post('/',async(req,res)=>{
@@ -15,16 +15,15 @@ router.post('/',async(req,res)=>{
     });
 })
 
-router.post('/',async(req,res)=>{
-    Station.collection.insertOne(req.body)
-    .then((result) => {
-        res.status(201).json(req.body);
-    })
-    .catch((err) => {
-        // Log any errors that occurred
-        console.log(err);
-    });
-})
+router.get('/', async (req, res) => {
+    Station.find()
+        .then((stations) => {
+            res.status(200).json({ stations });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
 
 
